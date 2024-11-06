45719458115b50c05d8e2b53a7583983e2da17c7
mohammed ouchen <medouchen19@gmail.com>
2024-11-06 15:39:34 +0100

    add Section

--- app.js
+++ app.js
@@ -9,12 +9,33 @@ function PageTransition (){
     for (let index = 0; index < sectBtn.length; index++) {
         sectBtn[index].addEventListener('click', function () {
             let currentBtn = document.querySelectorAll('.active-btn')
-            console.log('currentBtn => ', currentBtn[0].classList, currentBtn[0].classList);
+            // console.log('currentBtn => ', currentBtn[0].classList, currentBtn[0].classList);
             currentBtn[0].className = currentBtn[0].className.replace('active-btn', '')
             this.className += ' active-btn'
         })
-
     }
+
+
+    allSections.addEventListener('click', e => {
+        const id = e.target.dataset.id
+
+        if (id) {
+            sectBtns.forEach(btn => {
+                btn.classList.remove('active')
+            })
+
+            e.target.classList.add('active')
+
+            sections.forEach((section) => {
+                section.classList.remove('active')
+            })
+
+            const element = document.getElementById(id)
+
+            element.classList.add('active')
+
+        }
+    })
 }
 
 PageTransition()
