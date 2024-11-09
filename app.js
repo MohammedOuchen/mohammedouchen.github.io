56f9ab20f86abb8fd351c4485c9c71bf6e758d65
mohammed ouchen <medouchen19@gmail.com>
2024-11-09 14:45:39 +0100

    add home page

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ function PageTransition (){
     for (let index = 0; index < sectBtn.length; index++) {
         sectBtn[index].addEventListener('click', function () {
             let currentBtn = document.querySelectorAll('.active-btn')
-            // console.log('currentBtn => ', currentBtn[0].classList, currentBtn[0].classList);
             currentBtn[0].className = currentBtn[0].className.replace('active-btn', '')
             this.className += ' active-btn'
         })
