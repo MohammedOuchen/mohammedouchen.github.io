fbc6bb00c10898cb557234a69d2a5d0d835b6431
mohammed ouchen <medouchen19@gmail.com>
2024-11-21 15:30:00 +0100

    add contact me section and light mode

--- app.js
+++ app.js
@@ -35,6 +35,14 @@ function PageTransition (){
 
         }
     })
+
+
+    // toggle theme
+    const themeBtn = document.querySelector('.theme-btn');
+    themeBtn.addEventListener('click', () => {
+        let element = document.body
+        element.classList.toggle('light-mode')
+    })
 }
 
 PageTransition()
